bd8663ca35c17b07ad05acf089081578d9b1bc27
shyfan <410713847@qq.com>
2023-01-31 17:37:09 +0800

    article 16 end

---
+++ Platform/Windows/cbuffer2.h
@@ -0,0 +1,23 @@
+#ifndef __STDCBUFFER_H__
+#define __STDCBUFFER_H__
+
+struct a2v
+{
+    float3 Position : POSITION;
+    float3 Normal : NORMAL;
+    float4 Tangent : TANGENT;
+    float2 TextureUV : TEXCOORD;
+};
+
+cbuffer Constants : register(b0)
+{
+    float4x4 m_model;
+    float4x4 m_view;
+    float4x4 m_projection;
+    float4 m_lightPosition;
+    float4 m_lightColor;
+    float4 m_ambientColor;
+    float4 m_lightAttenuation;
+};
+
+#endif // !__STDCBUFFER_H__
